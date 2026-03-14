a255f06f5750b96af0b53837febaf10bd8592613
OctavioSanchez-lang <octaviosanchez247@gmail.com>
2026-03-14 04:27:40 +0000

    Revisión del ejercicio dos y modificaciones para el ejercicio uno.

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,26 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import data   
-import main   
+from data import read_data
+from main import similitud_geometrica
+from main import similitud
 import models
 
 #Ejercicio 1
 #La explicación de este ejercicio la pueden encontrar en README.md
-from data import read_data
-from main import similitud_geometrica
 
 longitud, peso = read_data("data/pescados.csv")
-<<<<<<< HEAD
-graficar_longitud_peso(longitud, peso)
+similitud(longitud, peso)
 
 # Ejercicio 2
 
-K = main.similitud_geometrica(longitud.tolist(), peso.tolist())
+K = similitud_geometrica(longitud.tolist(), peso.tolist())
 print(f"Valor de K estimado: {K:.8f}")
 x = longitud ** 3
 rho = models.pearson(x.tolist(), peso.tolist())
 print(f"Coeficiente de Pearson: {rho:.4f}")
 #Gráfica
 models.graficar_ej2(x=x.tolist(), y=peso.tolist(), k=K, titulo='Modelo de Similitud Geométrica: Peso vs Longitud al Cubo')
-=======
-
--- main.py
+++ main.py
@@ -6,12 +6,38 @@ import matplotlib.pyplot as plt
 """¿Eliminamos lo siguiente?
 from models import calc_error, modelo_geom, modelo_circ
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
+def similtud(longitud : list[float],  masa : list[float]):
+    longitud = np.array(longitud)
+    peso = np.array(masa)*9.81
+
+    l_prom = np.mean(longitud)
+    w_prom = np.mean(peso)
+
+    m = np.sum((longitud - l_prom)*(peso - w_prom)) / np.sum((longitud - l_prom)**2)
+    b = w_prom - m*l_prom
+
+    x = np.linspace(min(longitud), max(longitud), 100)
+    y = m*x + b
+
+    plt.scatter(longitud, peso, label="Datos")
+    plt.plot(x, y, label=f"Recta promedio: W = {m:.3f}l + {b:.3f}")
+
+    plt.xlabel("Longitud")
+    plt.ylabel("Peso")
+    plt.title("Relación Longitud vs Peso")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    return m, b
+
 
 def similitud_geometrica(longitud : list[float],  masa : list[float]):
-<<<<<<< HEAD
+
     """Calcula la constante K para el modelo W = K * l^3 usando Mínimos Cuadrados."""
-=======
-    
     
     # Convertimos las listas a arreglos de numpy
     l_array = np.array(longitud)
